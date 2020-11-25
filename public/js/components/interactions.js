7299eab0694c511a70b7d225496a772a6b5b2dfe
jtq <jamestpalmer1979@gmail.com>
2020-11-25 23:54:43 +0000

    Broke custom components into separate interactable and cosmetic libraries

--- public/js/components/interactions.js
+++ public/js/components/interactions.js
@@ -29,33 +29,6 @@ AFRAME.registerComponent('interactable', {
   play: function () {}
 });
 
-// Show object ina highlighted state
-AFRAME.registerComponent('halo', {
-  schema: {
-    highlight: {type: 'color', default: '#606060'}
-  },
-  init: function () {
-    this.el.object3D.traverse(obj => {
-      if(obj.material && typeof obj.material.originalEmissive === 'undefined') {
-        obj.material.originalEmissive = obj.material.emissive.getHex();
-        obj.material.emissive.set(this.data.highlight);
-        obj.material.originalEmissiveIntensity = obj.material.emissiveIntensity;
-        obj.material.emissiveIntensity = 0.5;
-      }
-    });
-  },
-  remove: function () {
-    this.el.object3D.traverse(obj => {
-      if(obj.material && (typeof obj.material.originalEmissive !== 'undefined')) {
-        obj.material.emissive.setHex(obj.material.originalEmissive);
-        obj.material.emissiveIntensity = obj.material.originalEmissiveIntensity;
-        delete(obj.material.originalEmissive);
-        delete(obj.material.originalEmissiveIntensity);
-      }
-    });
-  },
-});
-
 AFRAME.registerComponent('interaction', {
   dependencies: ['animation'],
   schema: {
@@ -152,37 +125,4 @@ AFRAME.registerComponent('selectable-goal', {
       e.stopPropagation();
     });
   }
-});
-
-
-
-
-
-AFRAME.registerComponent('show-origin', {
-  init: function () {
-    this.el.object3D.traverse(obj => {
-      if(obj instanceof THREE.Mesh && obj.material) {
-        obj.userData.originalOpacity = obj.material.opacity;
-        obj.material.opacity = 0.5;
-        obj.userData.originalTransparent = obj.material.transparent;
-        obj.material.transparent = true;
-      }
-    });
-    // Add origin-marker
-    const originMarker = document.createElement('a-sphere');
-    originMarker.setAttribute('radius', '0.05');
-    originMarker.setAttribute('position', '0 0 0');
-    originMarker.setAttribute('material', 'color', '#ff0000');
-    this.el.appendChild(originMarker);
-  },
-  remove: function () {
-    this.el.object3D.traverse(obj => {
-      if(obj.userData.originalOpacity || obj.userData.originalOpacity === 0) {
-        obj.material.opacity = obj.userData.originalOpacity;
-        delete(obj.userData.originalOpacity);
-        obj.material.transparent = obj.userData.originalTransparent;
-        delete(obj.userData.originalTransparent);
-      }
-    });
-  },
 });
